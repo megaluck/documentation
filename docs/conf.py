d33348ba3c133b698ab599f0f4f77e809363931b
Gustavo Fialho <gustavo.fialho@gmail.com>
2020-07-29 19:32:21 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -32,6 +32,7 @@ extensions = [
     'sphinx.ext.autosectionlabel',
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
+    'sphinx_tabs.tabs',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
